edd13500f30e2e9bac0a25e51c1af6d819d727c7
Major <telvinmaior@gmail.com>
2026-03-22 16:30:11 +0200

    add basename to BrowserRouter for proper routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>   {/* ← wraps App, inside StrictMode */}
+    <BrowserRouter basename='/examen-tibi/'>   {/* ← wraps App, inside StrictMode */}
       <App />
     </BrowserRouter>
   </StrictMode>
